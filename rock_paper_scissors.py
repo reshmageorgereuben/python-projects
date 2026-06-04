9d69b2a8e7c452fa422ef9a84ba8c0e1c733e030
Reshma George <reshmageorge44@gmail.com>
2026-06-04 03:23:20 -0400

    refactoring code

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,32 +1,50 @@
 import random
 
+
 choices = ("r","s","p")
 emojis = { 's': '✂️', 'p': '📄',  'r': '🪨'}
 
-while True:
-    user_choice = input("Rock, paper, or scissors? (r/p/s):")
-    computer_choice = random.choice(choices)
-
-    if(user_choice not in choices):
-        print("Invalid Choice!")
-    else:
-        print(f'You chose {emojis[user_choice]}')
-        print(f'Computer chose {emojis[computer_choice]}')
-
-        if user_choice == computer_choice:
-            print("Tie!")
-        elif (
-            (user_choice == 'r' and computer_choice == 's') or
-            (user_choice == 's' and computer_choice == 'p') or
-            (user_choice == 'p' and computer_choice == 'r')) :
-            print("You win")
+def get_user_choice():
+    while True:
+        user_choice = input("Rock, paper, or scissors? (r/p/s):")
+        if(user_choice in choices):
+            return user_choice
         else:
-            print("You lose")
+            print("Invalid Choice!")
+
+def display_choices(user_choice,computer_choice):
+    print(f'You chose {emojis[user_choice]}')
+    print(f'Computer chose {emojis[computer_choice]}')
 
 
-    should_continue = input("Continue? (y/n): ").lower()
-    if should_continue == "n":
-        break
+def determine_winner(user_choice,computer_choice):
+    if user_choice == computer_choice:
+        print("Tie!")
+    elif (
+        (user_choice == 'r' and computer_choice == 's') or
+        (user_choice == 's' and computer_choice == 'p') or
+        (user_choice == 'p' and computer_choice == 'r')) :
+        print("You win")
+    else:
+        print("You lose")
+
+
+def play_game():
+    while True:
+        user_choice = get_user_choice()
+        computer_choice = random.choice(choices)
+
+        
+        display_choices(user_choice,computer_choice)
+        determine_winner(user_choice,computer_choice)       
+
+
+        should_continue = input("Continue? (y/n): ").lower()
+        if should_continue == "n":
+            break
+
+
+play_game()
 
 
 
